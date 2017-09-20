0ba3976ec0207f5937ac89bbb7194132f88a043a
assalvatierra <assalvatierra@gmail.com>
2017-09-20 21:32:35 +0800

    partial db design

---
+++ scm/scm/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace scm
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
