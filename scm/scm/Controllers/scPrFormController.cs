7ea101c011950dd2c0d87b2d3c44c73355c3a353
Abel S. Salvatierra <assalvatierra@gmail.com>
2017-10-14 11:02:43 +0800

    master-detail.update

--- scm/scm/Controllers/scPrFormController.cs
+++ scm/scm/Controllers/scPrFormController.cs
@@ -52,6 +52,37 @@ namespace scm.Controllers
             return View(scPrHdr);
         }
 
+        // GET: scPrHdrs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            scPrHdr scPrHdr = db.scPrHdrs.Find(id);
+            if (scPrHdr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scPrHdr);
+        }
+
+        // POST: scPrHdrs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,dtPr,Remarks,Status")] scPrHdr scPrHdr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scPrHdr).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(scPrHdr);
+        }
+
         #endregion
 
         #region Item Functions
@@ -62,7 +93,7 @@ namespace scm.Controllers
             var newitem = new scPrDtl();
             newitem.scPrHdrId = hdrid;
 
-            ViewBag.scPrHdrId = new SelectList(db.scPrHdrs, "Id", "Remarks");
+            ViewBag.scPrHdrId = new SelectList(db.scPrHdrs, "Id", "Remarks",hdrid);
             ViewBag.scItemId = new SelectList(db.scItems, "Id", "Name");
             ViewBag.scUomId = new SelectList(db.scUoms, "Id", "Unit");
             return View(newitem);
