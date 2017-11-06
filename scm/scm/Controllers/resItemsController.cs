435b20a8a49bea27e05f68e6cc93465e175ff97b
Abel S. Salvatierra <assalvatierra@gmail.com>
2017-11-06 17:39:26 +0800

    etc.updates
    retail details

---
+++ scm/scm/Controllers/resItemsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using scm.Models;
+
+namespace scm.Controllers
+{
+    public class resItemsController : Controller
+    {
+        private ScmDBContainer db = new ScmDBContainer();
+
+
+        public ActionResult EditItem(int? id)
+        {
+            Models.scItem item = db.scItems.Find(id);
+            Models.resItem data = db.resItems.Where(d => d.scItemId == id).FirstOrDefault();
+
+            if(data==null)
+            {
+                data = new resItem();
+                data.scItemId = (int) id;
+                data.Description = item.Name;
+                data.resQty = 1;
+
+                db.resItems.Add(data);
+                db.SaveChanges();
+            }
+
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditItem([Bind(Include = "Id,scItemId,Description,Price,barcode,resQty")] resItem resItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(resItem);
+        }
+
+
+
+    }
+}
