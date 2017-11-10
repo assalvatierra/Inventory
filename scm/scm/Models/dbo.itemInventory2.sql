8e34f7bb7ffe68db90e340b42b14e9f612540f86
Abel S. Salvatierra <assalvatierra@gmail.com>
2017-11-10 14:50:16 +0800

    sql partial

--- scm/scm/Models/dbo.itemInventory2.sql
+++ scm/scm/Models/dbo.itemInventory2.sql
@@ -1,17 +1,35 @@
-﻿	SELECT
-	a.Id,
-	max(isnull(a.Name, '')) itemname,
-	sum(isnull(b.Qty, 0)) + sum(isnull(d.itemQty, 0)) ItemIn,
-	sum(isnull(c.Qty, 0)) + sum(isnull(e.Qty, 0)) ItemOut,
-	Max(isnull(a.LowLevel,0)) LowLevel
-	from [dbo].[scItems] a
-	left outer join [dbo].[scRcvDtls]
-			b on b.scItemId = a.Id
-	left outer join [dbo].[scInvOutDtls]
-			c on c.scItemId = a.Id
-	left outer join [dbo].[resPreparations]
-			d on d.scItemId = a.Id
-	left outer join [dbo].[resPrepMaterials]
-			e on e.scItemId = a.Id
+﻿
+	select 'PO' as tType, a.Id, a.Name, 
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as d0,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as d1,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as d2,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as d3,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as d4,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as d5,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as d6,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as d7
+	from scItems a
+		left outer join scPoDtls b on b.scItemId = a.Id
+		left outer join scPoHdrs c on c.Id = b.scPoHdrId
+	
+	union
 
-	Group by a.Id;
+	select 'SO' as tType, a.Id, a.Name, 
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as d0,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as d1,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as d2,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as d3,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as d4,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as d5,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as d6,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as d7
+	from scItems a
+		left outer join resItems ax on ax.scItemId = a.Id
+		left outer join resOrderDtls b on b.resItemId = ax.Id
+		left outer join resOrderHdrs c on c.Id = b.resOrderHdrId
+	;
+
+
+
+
+
