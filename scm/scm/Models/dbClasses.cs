2c8eaa8549b9f0fe4404797d3d3374d9e743da71
Abel S. Salvatierra <assalvatierra@gmail.com>
2017-11-14 13:43:05 +0800

    itemInventory2

--- scm/scm/Models/dbClasses.cs
+++ scm/scm/Models/dbClasses.cs
@@ -36,6 +36,27 @@ namespace scm.Models
         public DateTime Expirydate { get; set; }
     }
 
+    public class EntItems2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal p0 { get; set; }
+        public decimal s0 { get; set; }
+        public decimal p1 { get; set; }
+        public decimal s1 { get; set; }
+        public decimal p2 { get; set; }
+        public decimal s2 { get; set; }
+        public decimal p3 { get; set; }
+        public decimal s3 { get; set; }
+        public decimal p4 { get; set; }
+        public decimal s4 { get; set; }
+        public decimal p5 { get; set; }
+        public decimal s5 { get; set; }
+        public decimal p6 { get; set; }
+        public decimal s6 { get; set; }
+        public decimal p7 { get; set; }
+        public decimal s7 { get; set; }
+    }
     //public class EntItemSupplier
     //{
     //    public int Id { get; set; }
@@ -133,6 +154,88 @@ namespace scm.Models
                 return list.Where(d => d.ItemId == (int)id).ToList();
 
             return list.ToList();
+        }
+
+        public List<EntItems2> getItemInventory2()
+        {
+            #region Sql Statement
+            string sSQL = @"
+select max(x.id) Id,
+	max(x.Name) Name,
+	CAST(SUM(x.p0) as DECIMAL) P0, CAST(SUM(x.s0) as DECIMAL) s0,
+	CAST(SUM(x.p1) as DECIMAL) P1, CAST(SUM(x.s1) as DECIMAL) s1,
+	CAST(SUM(x.p2) as DECIMAL) P2, CAST(SUM(x.s2) as DECIMAL) s2,
+	CAST(SUM(x.p3) as DECIMAL) P3, CAST(SUM(x.s3) as DECIMAL) s3,
+	CAST(SUM(x.p4) as DECIMAL) P4, CAST(SUM(x.s4) as DECIMAL) s4,
+	CAST(SUM(x.p5) as DECIMAL) P5, CAST(SUM(x.s5) as DECIMAL) s5,
+	CAST(SUM(x.p6) as DECIMAL) P6, CAST(SUM(x.s6) as DECIMAL) s6,
+	CAST(SUM(x.p7) as DECIMAL) P7, CAST(SUM(x.s7) as DECIMAL) s7
+ from 
+(
+
+    SELECT 'CUR' as tType, a.Id, max(isnull(a.Name, '')) Name,
+    sum(isnull(b.Qty, 0)) 
+		+ sum(isnull(d.itemQty, 0)) 
+		- sum(isnull(c.Qty, 0)) 
+		+ sum(isnull(e.Qty, 0)) as p0, '0' as s0,
+	'0' as p1, '0' as s1,
+	'0' as p2, '0' as s2,
+	'0' as p3, '0' as s3,
+	'0' as p4, '0' as s4,
+	'0' as p5, '0' as s5,
+	'0' as p6, '0' as s6,
+	'0' as p7, '0' as s7
+    from [dbo].[scItems] a
+    left outer join [dbo].[scRcvDtls]
+            b on b.scItemId = a.Id
+    left outer join [dbo].[scInvOutDtls]
+            c on c.scItemId = a.Id
+	left outer join [dbo].[resPreparations]
+			d on d.scItemId = a.Id
+	left outer join [dbo].[resPrepMaterials]
+			e on e.scItemId = a.Id
+	Group by a.Id
+
+	union
+
+	select 'PO' as tType, a.Id, a.Name, 
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as p0, '0' as s0,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as p1, '0' as s1,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as p2, '0' as s2,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as p3, '0' as s3,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as p4, '0' as s4,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as p5, '0' as s5,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as p6, '0' as s6,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as p7, '0' as s7
+	from scItems a
+		left outer join scPoDtls b on b.scItemId = a.Id
+		left outer join scPoHdrs c on c.Id = b.scPoHdrId
+	
+	union
+
+	select 'SO' as tType, a.Id, a.Name, 
+	'0' as p0, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as s0,
+	'0' as p1, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as s1,
+	'0' as p2, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as s2,
+	'0' as p3, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as s3,
+	'0' as p4, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as s4,
+	'0' as p5, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as s5,
+	'0' as p6, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as s6,
+	'0' as p7, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as s7
+	from scItems a
+		left outer join resItems ax on ax.scItemId = a.Id
+		left outer join resOrderDtls b on b.resItemId = ax.Id
+		left outer join resOrderHdrs c on c.Id = b.resOrderHdrId
+
+) x
+group by x.Id
+
+";
+            #endregion
+
+            var list = db.Database.SqlQuery<EntItems2>(sSQL);
+            return list.ToList();
+
         }
 
         public void executeSQL(string sql)
--- scm/scm/Models/dbo.itemInventory2.sql
+++ scm/scm/Models/dbo.itemInventory2.sql
@@ -1,13 +1,50 @@
-﻿
+﻿select max(x.id) Id,
+	max(x.Name) Name,
+	CAST(SUM(x.p0) as DECIMAL) P0, CAST(SUM(x.s0) as DECIMAL) s0,
+	CAST(SUM(x.p1) as DECIMAL) P1, CAST(SUM(x.s1) as DECIMAL) s1,
+	CAST(SUM(x.p2) as DECIMAL) P2, CAST(SUM(x.s2) as DECIMAL) s2,
+	CAST(SUM(x.p3) as DECIMAL) P3, CAST(SUM(x.s3) as DECIMAL) s3,
+	CAST(SUM(x.p4) as DECIMAL) P4, CAST(SUM(x.s4) as DECIMAL) s4,
+	CAST(SUM(x.p5) as DECIMAL) P5, CAST(SUM(x.s5) as DECIMAL) s5,
+	CAST(SUM(x.p6) as DECIMAL) P6, CAST(SUM(x.s6) as DECIMAL) s6,
+	CAST(SUM(x.p7) as DECIMAL) P7, CAST(SUM(x.s7) as DECIMAL) s7
+ from 
+(
+
+    SELECT 'CUR' as tType, a.Id, max(isnull(a.Name, '')) Name,
+    sum(isnull(b.Qty, 0)) 
+		+ sum(isnull(d.itemQty, 0)) 
+		- sum(isnull(c.Qty, 0)) 
+		+ sum(isnull(e.Qty, 0)) as p0, '0' as s0,
+	'0' as p1, '0' as s1,
+	'0' as p2, '0' as s2,
+	'0' as p3, '0' as s3,
+	'0' as p4, '0' as s4,
+	'0' as p5, '0' as s5,
+	'0' as p6, '0' as s6,
+	'0' as p7, '0' as s7
+    from [dbo].[scItems] a
+    left outer join [dbo].[scRcvDtls]
+            b on b.scItemId = a.Id
+    left outer join [dbo].[scInvOutDtls]
+            c on c.scItemId = a.Id
+	left outer join [dbo].[resPreparations]
+			d on d.scItemId = a.Id
+	left outer join [dbo].[resPrepMaterials]
+			e on e.scItemId = a.Id
+	Group by a.Id
+
+	union
+
 	select 'PO' as tType, a.Id, a.Name, 
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as d0,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as d1,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as d2,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as d3,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as d4,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as d5,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as d6,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as d7
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as p0, '0' as s0,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as p1, '0' as s1,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as p2, '0' as s2,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as p3, '0' as s3,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as p4, '0' as s4,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as p5, '0' as s5,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as p6, '0' as s6,
+	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as p7, '0' as s7
 	from scItems a
 		left outer join scPoDtls b on b.scItemId = a.Id
 		left outer join scPoHdrs c on c.Id = b.scPoHdrId
@@ -15,18 +52,21 @@
 	union
 
 	select 'SO' as tType, a.Id, a.Name, 
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as d0,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as d1,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as d2,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as d3,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as d4,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as d5,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as d6,
-	IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as d7
+	'0' as p0, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 0, b.Qty, 0 ) as s0,
+	'0' as p1, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 1, b.Qty, 0 ) as s1,
+	'0' as p2, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 2, b.Qty, 0 ) as s2,
+	'0' as p3, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 3, b.Qty, 0 ) as s3,
+	'0' as p4, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 4, b.Qty, 0 ) as s4,
+	'0' as p5, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 5, b.Qty, 0 ) as s5,
+	'0' as p6, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 6, b.Qty, 0 ) as s6,
+	'0' as p7, IIF( DATEDIFF(day,GETDATE(),c.dtDelivery) = 7, b.Qty, 0 ) as s7
 	from scItems a
 		left outer join resItems ax on ax.scItemId = a.Id
 		left outer join resOrderDtls b on b.resItemId = ax.Id
 		left outer join resOrderHdrs c on c.Id = b.resOrderHdrId
+
+) x
+group by x.Id
 	;
 
 
